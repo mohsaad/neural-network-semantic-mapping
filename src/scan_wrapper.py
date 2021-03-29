ef43d20707ded0d702c43fd01b063c3cb1580dc2
mohsaad <msaad96@gmail.com>
2021-03-28 23:29:13 -0400

    scan_wrapper: Wrote pose loading function

--- src/scan_wrapper.py
+++ src/scan_wrapper.py
@@ -2,10 +2,12 @@
 """
 Class to load scans into ros and send them in at a fixed rate.
 """
-import rospy
-from std_msgs.msg import String
+import argparse
 import numpy as np
 
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Pose
 
 # Note: this depends on the structure of our project
 def load_scan(filename):
@@ -25,12 +27,35 @@ def load_scan(filename):
     return point_cloud, pose
 
 
+def load_poses_from_file(filename):
+    scan_poses = []
+    poses_raw = np.genfromtxt(filename, delimiter=' ')
+    for idx in range(0, poses_raw.shape[0]):
+        pose = np.eye(4)
+        pose[0, 0:4] = poses_raw[idx, 0:4]
+        pose[1, 0:4] = poses_raw[idx, 4:8]
+        pose[2, 0:4] = poses_raw[idx, 8:12]
+        pose[3, 3] = 1.0
+
+        # TODO: Apply calibration
+        scan_poses.append(pose)
+
+    return scan_poses
+
+
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', "--poses", required=True, help="path to poses file")
+
+    args = parser.parse_args()
+
     lidar_publisher = rospy.Publisher("point_cloud", String, queue_size=10)
     pose_publisher = rospy.Publisher("pose", String, queue_size=10)
 
     rospy.init_node("scan_wrapper")
     rate = rospy.Rate(10)
+
+    load_poses_from_file(args.poses)
 
     counter = 0
 
